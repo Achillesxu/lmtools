3106dbd63abf7e7d40e3ee01ee972769edfc9137
Achillesxu <yuqingxushiyin@gmail.com>
2024-07-07 15:56:46 +0800

    Add big-lama file to Git LFS

---
+++ st_paint/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@Time : 2024 7月 01 20:45
+@Author : xushiyin
+@Mobile : 18682193124
+@desc :
+"""
---
+++ st_paint/paint.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@Time : 2024 7月 01 20:47
+@Author : xushiyin
+@Mobile : 18682193124
+@desc :
+"""
